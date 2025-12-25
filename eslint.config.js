495b98f9279b0d70bc3897bf16da35001d2b1f72
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-25 17:08:28 +0700

    linter

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginVue.configs['flat/essential'],
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json' },
+]);
