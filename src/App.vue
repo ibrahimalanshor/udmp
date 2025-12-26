e724d01020bfa70fb51c49cd1db4b6b6784e8fb6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-26 16:21:26 +0700

    feat: empty state

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,7 @@
 import { ref } from 'vue';
 import { Icon } from '@iconify/vue';
 
-const todos = ref([
-  { id: 1, name: 'Menyelesaikan desain UI', done: false },
-  { id: 2, name: 'Beli kopi & susu', done: false },
-  { id: 3, name: 'Meeting tim pagi', done: true },
-  { id: 4, name: 'Bayar tagihan internet', done: true },
-]);
+const todos = ref([]);
 </script>
 
 <template>
@@ -30,14 +25,41 @@ const todos = ref([
             Today
           </h1>
         </div>
-        <a
-          href=""
-          class="block w-10 h-10 border border-neutral-200 text-neutral-900 rounded-full flex items-center justify-center transition hover:bg-black hover:border-black hover:text-white"
-        >
-          <Icon icon="ri:github-fill" class="size-6" />
-        </a>
+        <div class="flex items-center gap-2">
+          <a
+            href=""
+            class="block w-10 h-10 border border-neutral-200 text-neutral-900 rounded-full flex items-center justify-center transition hover:bg-black hover:border-black hover:text-white"
+          >
+            <Icon icon="ri:github-fill" class="size-6" />
+          </a>
+          <a
+            href=""
+            class="block w-10 h-10 border border-neutral-200 text-neutral-900 rounded-full flex items-center justify-center transition hover:bg-black hover:border-black hover:text-white"
+          >
+            <Icon icon="ri:moon-fill" class="size-5" />
+          </a>
+        </div>
       </div>
-      <div class="p-4 space-y-1">
+      <div
+        v-if="!todos.length"
+        class="p-4 text-center min-h-80 flex flex-col justify-center items-center gap-4"
+      >
+        <div
+          class="w-18 h-18 bg-neutral-50 rounded-full flex items-center justify-center"
+        >
+          <Icon
+            icon="ri:checkbox-circle-line"
+            class="size-10 text-neutral-300"
+          />
+        </div>
+        <div>
+          <h2 class="text-lg font-semibold text-neutral-900">All Done</h2>
+          <p class="text-neutral-400 leading-relaxed">
+            No tasks for today, add some tasks below.
+          </p>
+        </div>
+      </div>
+      <div v-else class="p-4 space-y-1">
         <label
           v-for="(todo, index) of todos"
           :key="todo.id"
@@ -67,7 +89,7 @@ const todos = ref([
           </p>
         </label>
       </div>
-      <div class="p-6 border-t border-neutral-200">
+      <div class="p-6 border-t border-neutral-100">
         <input
           type="text"
           class="h-13 px-5 w-full border border-transparent rounded-2xl bg-neutral-50 transition placeholder:text-neutral-400 focus:bg-white focus:outline-none focus:border-neutral-300"
